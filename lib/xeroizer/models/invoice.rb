efa04f044c1e68d03fa413368af2cd4d77f3e903
Jackson Lin <jacksonlin926@gmail.con>
2017-07-22 10:34:28 +1000

    * added overpayments

--- lib/xeroizer/models/invoice.rb
+++ lib/xeroizer/models/invoice.rb
@@ -86,6 +86,7 @@ module Xeroizer
       has_many     :line_items, :complete_on_page => true
       has_many     :payments
       has_many     :credit_notes
+      has_many     :overpayments
 
       validates_presence_of :date, :due_date, :unless => :new_record?
       validates_inclusion_of :type, :in => INVOICE_TYPES
